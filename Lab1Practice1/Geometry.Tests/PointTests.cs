511ca508545b90fda33e5c718bdd259e3c1dbc9d
BETAKOTLETA <viktor.oleniuk@gmail.com>
2025-03-12 15:13:31 +0200

    Updated PointTests and Segments

--- Lab1Practice1/Geometry.Tests/PointTests.cs
+++ Lab1Practice1/Geometry.Tests/PointTests.cs
@@ -5,20 +5,58 @@ namespace Geometry.Tests
     public class PointTests
     {
         [Fact]
+        public void Point_can_be_created()
+        {
+            var Testpoint = new Point();
+
+            Testpoint.Should().BeOfType<Point>();
+
+            Testpoint.X.Should().Be(0);
+            Testpoint.Y.Should().Be(0);
+
+            
+        }
+        [Fact]
+        public void Point_can_be_created_with_one_input()
+        {
+            var Testpoint = new Point(2);
+
+            Testpoint.Should().BeOfType<Point>();
+
+            Testpoint.X.Should().Be(2);
+            Testpoint.Y.Should().Be(2);
+
+
+        }
+        [Fact]
+        public void Point_can_be_created_with_two_inputs()
+        {
+            var Testpoint = new Point(1, 2);
+
+            Testpoint.Should().BeOfType<Point>();
+
+            Testpoint.X.Should().Be(1);
+            Testpoint.Y.Should().Be(2);
+        }
+        [Fact]
         public void Move_should_return_proper_results()
         {
-            var TestPoint = new Point(5, 5);
-            TestPoint.Move(-5, -4);
+            var Testpoint = new Point(5, 5);
 
-            TestPoint.X.Should().Be(0);
-            TestPoint.Y.Should().Be(1);
+            Testpoint.Move(-5, -4);
+
+            Testpoint.Should().BeOfType<Point>();
+
+            Testpoint.X.Should().Be(0);
+            Testpoint.Y.Should().Be(1);
         }
         [Fact]
         public void Distance_should_return_proper_results()
         {
-            var TestPoint = new Point(1, 2);
+            var Testpoint = new Point(3, 4);
 
-            TestPoint.Distance().Should().Be(2.23606797749979);
+            Testpoint.Should().BeOfType<Point>();
+            Testpoint.Distance().Should().Be(5);
         }
 
     }
--- Lab1Practice1/Geometry.Tests/SegmentTests.cs
+++ Lab1Practice1/Geometry.Tests/SegmentTests.cs
@@ -4,14 +4,29 @@ namespace Geometry.Tests
 {
     public class SegmentTests
     {
+
+
         [Fact]
         public void Length_should_return_proper_results()
         {
             var TestSegment = new Segment(new Point(1, 1), new Point(4, 5));
 
-            TestSegment.Length.Should().Be(5); // I am not sure about this value
+            TestSegment.Length.Should().Be(5); 
         }
 
+        [Fact]
+        public void Segment_can_be_created()
+        {
+            var TestSegment = new Segment(new Point(1, 1), new Point(4, 5));
 
+            TestSegment.Should().BeOfType<Segment>();
+
+
+
+            TestSegment.Start.Should().Be(new Point(1, 1));
+            TestSegment.Start.Should().BeOfType<Point>();
+            TestSegment.End.Should().Be(new Point(4, 5));
+            TestSegment.End.Should().BeOfType<Point>();
+        }
     }
 }
