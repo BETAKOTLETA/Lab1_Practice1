915b32cff81cc097417b2334e4be670d9bc3dd00
BETAKOTLETA <viktor.oleniuk@gmail.com>
2025-03-12 14:40:36 +0200

    Bugs

--- Lab1Practice1/Geometry.Tests/PointTests.cs
+++ Lab1Practice1/Geometry.Tests/PointTests.cs
@@ -1,24 +1,50 @@
 using FluentAssertions;
+using Geometry;
+using Xunit;
 
 namespace Geometry.Tests
 {
     public class PointTests
     {
         [Fact]
+        public void Point_can_be_created()
+        {
+            var Testpoint = new Point();
+
+            Testpoint.X.Should().Be(0);
+            Testpoint.Y.Should().Be(0);
+        }
+        [Fact]
+        public void Point_can_be_created_with_one_input()
+        {
+            var Testpoint = new Point(2);
+
+            Testpoint.X.Should().Be(2);
+            Testpoint.Y.Should().Be(2);
+        }
+        [Fact]
+        public void Point_can_be_created_with_two_inputs()
+        {
+            var Testpoint = new Point(1, 2);
+
+            Testpoint.X.Should().Be(1);
+            Testpoint.Y.Should().Be(2);
+        }
+        [Fact]
         public void Move_should_return_proper_results()
         {
-            var TestPoint = new Point(5, 5);
-            TestPoint.Move(-5, -4);
+            var Testpoint = new Point(5, 5);
+            Testpoint.Move(-5, -4);
 
-            TestPoint.X.Should().Be(0);
-            TestPoint.Y.Should().Be(1);
+            Testpoint.X.Should().Be(0);
+            Testpoint.Y.Should().Be(1);
         }
         [Fact]
         public void Distance_should_return_proper_results()
         {
-            var TestPoint = new Point(1, 2);
+            var Testpoint = new Point(3, 4);
 
-            TestPoint.Distance().Should().Be(2.23606797749979);
+            Testpoint.Distance().Should().Be(5);
         }
 
     }
---
+++ Lab1Practice1/Main/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using System.Drawing;
+
+Console.WriteLine("Hello, World!");
+
+var point = new Point(1, 2);
+
+Console.WriteLine(point.Distance());
